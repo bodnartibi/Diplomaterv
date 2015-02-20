e76f930ec370c4cb32ee0cb9610851f6d0d6264b
bodnartibi <piratetibi@gmail.com>
2015-02-20 02:25:12 +0100

    right path on my PC

--- Device-tree/build_dtb.sh
+++ Device-tree/build_dtb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./dtc/dtc -I dts -O dtb -o ./devicetree.dtb ./my_device_tree.dts 
+/home/tibor/Dipterv/linux-xlnx/scripts/dtc/dtc -I dts -O dtb -o ./devicetree.dtb ./my_device_tree.dts 
 
