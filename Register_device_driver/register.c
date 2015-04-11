29efce3a2b170c1fa3b9749eeb5e300fcb063ed4
bodnartibi <piratetibi@gmail.com>
2015-04-11 20:52:23 +0200

    io mem dolgok hozzaadva, meg nem tesztelt rendes hibakezeles meg nincs

--- Register_device_driver/register.c
+++ Register_device_driver/register.c
@@ -1,7 +1,5 @@
 /* Kernel Programming */
-#define MODULE
 #define LINUX
-#define __KERNEL__
 
 #include <linux/module.h>  /* Needed by all modules */
 #include <linux/kernel.h>  /* Needed for KERN_ALERT */
@@ -16,18 +14,24 @@
 #include <linux/fcntl.h> /* O_ACCMODE */
 #include <asm/uaccess.h> /* copy_from/to_user */
 #include <linux/platform_device.h>
+#include <asm/io.h>
 
 #define BUFF_SIZE 16
+//TODO ne beégetett címekkel
+#define MYREG_START 0x75c0000
+#define MYREG_LEN 0x10000
+
 int reg_open(struct inode *inode, struct file *filp);
 int reg_release(struct inode *inode, struct file *filp);
-ssize_t reg_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
-ssize_t reg_write(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+static ssize_t reg_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+static ssize_t reg_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos);
 
 //Global
 
 int testreg_major = 200;
 int reg[1];
 char *input_buffer;
+void *regs;
 
 struct file_operations testreg_fops = {
   read: reg_read,
@@ -48,14 +52,16 @@ int reg_release(struct inode *inode, struct file *filp)
 }
 
 
-ssize_t reg_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
+static ssize_t reg_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
 
   /* Transfering data to user space */
   input_buffer[BUFF_SIZE -1] = '\0';
   copy_to_user(buf, input_buffer, BUFF_SIZE);
   printk("<1> read %s\n", input_buffer);
-  
+
+	//TODO ioread 
+ 
   /* Changing reading position as best suits */
   if (*f_pos == 0) {
     *f_pos += BUFF_SIZE;
@@ -66,9 +72,8 @@ ssize_t reg_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 }
 
 
-ssize_t reg_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
+static ssize_t reg_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos)
 {
-  char *tmp;
   int c;
   //tmp = buf + count - 1;
 
@@ -82,11 +87,15 @@ ssize_t reg_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
   copy_from_user(input_buffer, buf, c);
   //f_pos += c;
   printk("write %s\n", input_buffer);
+
+	//TODO iowrite
+
   return c;
 }
 
 static int myregister_remove(struct platform_device *pdev)
 {
+	//TODO TODO ide az i/o dolgokat is!
   /* Freeing the major number */
   unregister_chrdev(testreg_major, "myreg");
 
@@ -102,7 +111,6 @@ static int myregister_remove(struct platform_device *pdev)
 static int myregister_probe(struct platform_device *pdev)
 {
   int result;
-  const struct of_device_id *match;
   
   printk("<1>Probe start\n");
 
@@ -122,14 +130,37 @@ static int myregister_probe(struct platform_device *pdev)
   input_buffer = kmalloc(BUFF_SIZE, GFP_KERNEL); 
   if (!input_buffer) { 
     result = -ENOMEM;
-    goto fail; 
+    goto fail_req; 
   } 
   memset(input_buffer, 0, BUFF_SIZE);
 
+	//TODO ne beégetett címmel/hosszal
+	if(!(check_mem_region(MYREG_START, MYREG_LEN))) {
+		printk("<1>ERROR check mem region\n");
+		goto fail_req;
+	}
+
+  //TODO ne beégetett címmel/hosszal
+  if (NULL == request_mem_region(MYREG_START, MYREG_LEN, "myreg_mem")) {
+  printk("<1>ERROR request mem region\n");
+  //TODO hibakezelés mert ez nem elég itt
+  // ezt jo helye void release_mem_region(unsigned long start, unsigned long len);
+    goto fail_req;
+  }  
+
+  regs = ioremap(MYREG_START, MYREG_LEN);
+  if(!regs) {
+     printk("<1>ERROR ioremap\n");
+  //TODO rendes hibakezelés
+    goto fail_map;
+  }
   printk("<1>Inserting myreg module\n"); 
   return 0;
 
-  fail: 
+	fail_map:
+	release_mem_region(MYREG_START, MYREG_LEN);
+  fail_req: 
+	  //TODO ez még így full rossz
   myregister_remove(pdev); 
   return result;	
   
