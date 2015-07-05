94e15c45130c4b8d171bea5a358ef9c36089b1a6
bodnartibi <piratetibi@gmail.com>
2015-07-06 01:19:07 +0200

    MATLAB: CIC and average filter

--- other/MATLAB_files/CIC.m
+++ other/MATLAB_files/CIC.m
@@ -10,8 +10,8 @@ cic_num(D+1) = -1;
 
 cic_den = [1 -1];
 
-cic_h = filt(cic_num,cic_den)
-[cic_imp_y,cic_imp_t] = impulse(cic_h)
+cic_h = filt(cic_num,cic_den,0.1)
+[cic_imp_y,cic_imp_t] = impulse(cic_h);
 
 % recursive average filter
 ave_num = zeros(1,D+1);
@@ -19,12 +19,18 @@ ave_num(1) = 1;
 ave_num(D+1) = -1;
 ave_den = D*[1 -1];
 
-ave_h = filt(ave_num,ave_den)
+ave_h = filt(ave_num,ave_den,0.1)
 [ave_imp_y,ave_imp_t] = impulse(ave_h);
 
 % plot
 figure(1)
-bode(cic_h), grid
+%subplot (2,1,1)
+bode(cic_h); grid;
+hold on
+%subplot (2,1,2)
+%figure(10)
+bode(ave_h); grid;
+hold off
 
 figure(2)
 plot(step(cic_h),'r')
@@ -32,9 +38,6 @@ hold on
 plot(step(ave_h),'g')
 legend('CIC','Average')
 hold off
-
-figure(4)
-bode(ave_h), grid
 
 figure(3)
 plot(cic_imp_y, 'r')
