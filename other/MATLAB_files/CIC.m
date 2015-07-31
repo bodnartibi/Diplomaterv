9bcb1abd710522ce1f55f696fe8d25ff7efae238
bodnartibi <piratetibi@gmail.com>
2015-07-31 23:14:06 +0200

    MATLAB - sigmadelta simulink

--- other/MATLAB_files/CIC.m
+++ other/MATLAB_files/CIC.m
@@ -1,9 +1,10 @@
 %%parameters
 clear;
 
-D = 25;
-
-N = 3;
+D = 10;
+Ts = 308*10^(-9) %308 ns, STMICROELECTRONICS
+%Ts = 308;
+N = 1;
 
 % CIC filter
 cic_num = zeros(1,D+1);
@@ -12,7 +13,7 @@ cic_num(D+1) = -1;
 
 cic_den = [1 -1];
 
-cic_h = filt(cic_num,cic_den,0.01)
+cic_h = filt(cic_num,cic_den,Ts)
 cic_h = cic_h^N;
 [cic_imp_y,cic_imp_t] = impulse(cic_h);
 
@@ -22,7 +23,7 @@ ave_num(1) = 1;
 ave_num(D+1) = -1;
 ave_den = D*[1 -1];
 
-ave_h = filt(ave_num,ave_den,0.01)
+ave_h = filt(ave_num,ave_den,Ts)
 [ave_imp_y,ave_imp_t] = impulse(ave_h);
 
 % plot
@@ -49,3 +50,19 @@ plot(ave_imp_y, 'bo')
 legend('CIC','Average')
 hold off
 title('Impulse responses')
+
+figure(4)
+
+[h,w] = freqz(cic_num,cic_den,'half',2001);
+plot(w/pi,20*log10(abs(h)),'r')
+grid;
+ax = gca;
+%ax.YLim = [-100 20];
+ax.XTick = 0:.25:1;
+xlabel('Normalized Frequency (\times\pi rad/sample)')
+ylabel('Magnitude (dB)')
+
+hold on;
+[h,w] = freqz(ave_num,ave_den,'half',2001);
+plot(w/pi,20*log10(abs(h)),'b')
+hold off;
