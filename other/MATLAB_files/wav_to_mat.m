11651653feff5cbfc5210b0bf036cbd412a1f4a0
bodnartibi <piratetibi@gmail.com>
2015-10-24 15:27:32 +0200

    MATLAB, FPGA: cic finomitas

--- other/MATLAB_files/wav_to_mat.m
+++ other/MATLAB_files/wav_to_mat.m
@@ -1,4 +1,7 @@
-[csett,fs] = wavread('csett.wav');
+function [csett] = wav_to_mat(in, out)
+% in: input .wav file path
+% out: output .mat file path
+[csett,fs] = wavread(in);
 figure(100)
 csett_t= [1:size(csett)];
 plot(csett_t,csett,'b')
@@ -7,4 +10,6 @@ csett= transpose(csett);
 csett(1,:)=csett_t;
 plot(csett_t,csett(2,:),'r');
 hold off
-save('csett.mat','csett')
+save(out,'csett')
+csett = transpose(csett(2,:));
+
